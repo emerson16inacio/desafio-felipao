5f62240248e4531828be3f7e76d24047fb657c98
emerson16inacio <netoinacio82@gmail.com>
2024-05-09 00:50:48 -0300

    atualização de projeto

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let  nome = "Emerson";
-console.log("Olá " + nome);
+let nome = "Emerson"
+let rank =  2001
+
+if(rank < 1000  ){
+console.log("O Herói de nome " + nome + " está no nível de Ferro")
+}else if(rank < 2001){
+console.log("O Herói de nome " + nome + " está no nível de Bronze")
+}else if(rank < 5001){
+console.log("O Herói de nome " + nome + " está no nível de Prata")
+}else if(rank < 7001){
+console.log("O Herói de nome " + nome + " está no nível de Ouro")
+}else if(rank < 8001){
+console.log("O Herói de nome " + nome + " está no nível de Platina")
+}else if(rank < 9001){
+console.log("O Herói de nome " + nome + " está no nível de Ascendente")
+}else if(rank < 10001){
+console.log("O Herói de nome " + nome + " está no nível de Imortal")
+}else if(rank >= 10001){
+console.log("O Herói de nome " + nome + " está no nível de Radiante")
+}else{
+console.log("Você não mencionou um número")
+}
